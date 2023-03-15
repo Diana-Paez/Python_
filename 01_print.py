6ec62452c3807966a8eeb09a17f40add63905f94
Diana Paez <developermerci@gmail.com>
2023-03-15 17:07:02 -0500

    Varaibles

---
+++ 01_print.py
@@ -0,0 +1,1 @@
+print("this is a copy of introduction.py")
---
+++ 02_vars.py
@@ -0,0 +1,26 @@
+print("Hola, vars")
+
+# this is a variable with a string
+my_name = "Nicolas"
+print(my_name)
+
+# this is a variable with Integer
+my_age = 12 
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio", my_name)
+
+# input
+
+my_name = input("What is your name?")
+print("the new value of variable my_name is: ",my_name)
+print(my_name)
+
+
+my_age = 35
+print("aqui cambio",my_age)
+
+my_age = input("What is your age?")
+print("the new value of variable my_age is:", my_age)
+print(my_age)
