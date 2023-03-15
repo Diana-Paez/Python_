ea498cf03a7030896a7764fd8ca15082bda7f73f
Diana Paez <developermerci@gmail.com>
2023-03-15 12:26:01 -0500

    Introduction

---
+++ fund_python_class1.py
@@ -0,0 +1,23 @@
+#Into python the indent is very important 
+
+print("Hello baby")
+
+""" 
+This
+is 
+a
+comment
+multiline
+"""
+
+''' 
+This
+is a comment
+multilin
+
+'''
+
+print(10 +5)
+print(40 - 30)
+print(100 / 5)
+print(5 * 20)
