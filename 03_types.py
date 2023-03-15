95e0059fb9f6d8c4f710a9db0620c5249b9bff91
Diana Paez <developermerci@gmail.com>
2023-03-15 17:47:03 -0500

    Type of data

---
+++ 03_types.py
@@ -0,0 +1,24 @@
+# Type of data
+#string
+my_name = "Juliana"
+my_name = 'Oracio'
+my_name = "12"
+print('my_name =>', my_name)
+print(type(my_name))
+
+# int
+my_age = 12
+print('my_age =>', my_age)
+print(type(my_age))
+
+# boolean
+
+is_single = False
+print("is_single =>", is_single)
+print(type(is_single))
+
+#input
+my_age = input('what is your age? ')
+print("my_age =>", my_age)
+print(type(my_age))
+
