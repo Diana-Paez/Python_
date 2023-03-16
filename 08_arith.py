c42528070d504899afb7e3dbe435b088c5bbd3d3
Diana Paez <developermerci@gmail.com>
2023-03-16 17:43:02 -0500

    operadores aritmeticos

---
+++ 08_arith.py
@@ -0,0 +1,18 @@
+#operadores aritmetico
+
+print(10 + 10)
+print(10 - 5)
+print(10 * 5)
+print(10 / 2)
+print(10 % 2) # el residuo de una division
+print(10 / 3)
+print(10 % 3)
+print(10 // 3) # divide y solo coge el entero
+print(2 ** 3) # potenciacion
+print( 2 ** 3 + 3 - 7 / 1 // 4)
+print(2 ** 3)
+print((7 / 1) // 4)
+print(8 + 3 - 1)
+
+print("Hola " * 3)
+print("hola" + " Diana")
