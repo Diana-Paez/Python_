36bfa0e754b7dbaa0f0fca61d784e39ba6d04643
Diana Paez <developermerci@gmail.com>
2023-03-16 18:20:23 -0500

    comparison between floats

---
+++ 09_comparison_between_floats.py
@@ -0,0 +1,16 @@
+# comparison between floats
+
+x = 3.3
+print(x)
+
+y = 1.1 + 2.2 
+print(y)
+print(x == y)
+
+y_str = format(y, ".2g")
+print("str => ", y_str)
+
+print("*" * 10)
+
+tolerance = 0.00001
+print(abs(x - y) < tolerance)
