c1aec66cc71b796eacb65bf7f9fa2a3314929eee
Diana Paez <developermerci@gmail.com>
2023-03-16 15:17:14 -0500

    Transformation

---
+++ 07_transformation.py
@@ -0,0 +1,26 @@
+#transformation
+
+name = "Viviana"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print("Nicolas" + " Salcedo")
+print(10+ 20)
+print("Nicolas"+ "33")
+
+age = 23
+print("My age is " + str(age))
+print(f"My age is {age}")
+
+age = input("Write to age => ")
+print(type(age))
+age = int(age)
+age += 10
+print(f" Tu edad en diez años sera {age}")
+
+
