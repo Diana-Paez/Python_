7190e16b7c4e0a3e0716a97f363072bde8641251
Diana Paez <developermerci@gmail.com>
2023-03-16 11:38:34 -0500

    integera and float

---
+++ 05_integer.py
@@ -0,0 +1,22 @@
+
+int_type = 33
+print(int_type)
+print(type(int_type))
+
+
+float_type = 55.8
+print(float_type)
+print(type(float_type))
+
+budget_total: int
+
+budget_january = 100000
+budget_febrary = 150000
+budget_may = 2000000
+
+budget_total = budget_january + budget_febrary + budget_may
+print(budget_total)
+
+print(f" The budget total is {budget_total}")
+
+
