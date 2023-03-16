96c9a4d9d9ec95494fdf91862ffbb8c3597ceba1
Diana Paez <developermerci@gmail.com>
2023-03-16 11:54:24 -0500

    Boolleans

---
+++ 06_booleans.py
@@ -0,0 +1,18 @@
+
+#booleans
+
+is_single = True
+print(type(is_single))
+
+is_married = False
+print(type(is_married))
+
+print(not is_married)
+
+print(not is_single)
+
+
+# el not es un operador de negacion
+# False equivale a 0
+#True equivale a 1
+
