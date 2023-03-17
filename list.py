4cf72e6da6957a402e1dc46efbbb69f43fba9658
Diana Paez <developermerci@gmail.com>
2023-03-17 16:28:38 -0500

    list

---
+++ list.py
@@ -0,0 +1,24 @@
+#list
+numbers = [1,2,3,4]
+print(numbers)
+print(type(numbers))
+
+tasks = ["make a dishes", "play videogames" ]
+print(tasks)
+
+types = [1, True, "hola"]
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+text = "Hola"
+text[0] = "w" # esto no se puede hacer
+# los strings son listas in mutables
+tasks[0] = "what platzi courses"
+print(tasks)
+
+tasks[0] = "d0 the dishes"
+print(tasks)
+
+
+ 
