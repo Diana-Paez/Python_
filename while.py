6d0fbf9b2356f23177535597cbeaabcb40467f38
Diana Paez <developermerci@gmail.com>
2023-03-23 09:23:02 -0500

    while

---
+++ while.py
@@ -0,0 +1,26 @@
+#while
+"""
+
+while True:
+    print('Se ejecuto"')
+
+"""
+counter = 0
+
+
+"""
+#break
+while counter < 20:
+    counter += 1
+    if counter == 15:
+        break
+    print(counter)
+"""
+
+#continue
+while counter < 20:
+    counter += 1
+    if counter < 15:
+        continue
+    print(counter)
+ 
